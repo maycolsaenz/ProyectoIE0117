3a2c2e484f928709927c1e51ae3b6cfbb09d01a5
maycolsaenz <maycol.saenz@ucr.ac.cr>
2022-07-04 22:18:37 -0600

    Orden a comentarios en el main.c

--- main.c
+++ main.c
@@ -5,18 +5,19 @@
 #include "soluciona.h"
 
 int main(){
-  
+  //Seccion 1: lectura del archivo laberinto.txt
+	
   char *informacion = malloc(10000*sizeof(char));
   FILE *archivo = fopen("./laberinto.txt", "r");
 
-  fread(informacion, 10000, sizeof informacion, archivo);
-  
+  fread(informacion, 10000*(sizeof *informacion), sizeof informacion, archivo);
+
   int col_len = cuenta_columnas(informacion);
   int fil_len = cuenta_filas(informacion, col_len);
-  node_t *HEAD = NULL;
+  
 
   
-//construccion del laberinto para analizar. Se guardara en el heap.
+//Seccion 2: construccion del laberinto para analizar. Se guardara en el heap.
 	int (*laberinto)[fil_len][col_len] = malloc(sizeof*laberinto); 
 
     for(int m = 0; m < fil_len; m++){
@@ -25,7 +26,7 @@ int main(){
     }
   }
 
-//se rellena la matrix solucion con ceros
+//Seccion 3: se rellena la matrix solucion con ceros
   int (*solucion)[fil_len][col_len] = malloc(sizeof*solucion);
   for(int i = 0; i < fil_len; i++){
     for(int j = 0; j < col_len; j++){
@@ -33,18 +34,20 @@ int main(){
     }
   }
 
-//Asignacion de coordenadas de entrada al laberinto por los lados
- int a;
- int b;
 
-
-// HEAD devuelve las coordenadas (x,y) de los unos en los bordes del laberinto como puntos de entrada. 
+// HEAD devuelve las coordenadas (x,y) de los unos en los bordes del laberinto como puntos de entrada.
+ node_t *HEAD = NULL;
  HEAD = par_ordenado(fil_len, col_len, laberinto);
 
   
-//Evaluacion de las entradas al laberinto
+//Seccion 4: Evaluacion de las entradas al laberinto
 //******************************  
-    while(HEAD != NULL){
+
+//Asignacion de coordenadas de entrada al laberinto por los lados
+ int a;
+ int b;
+	
+    while(HEAD != NULL){ //llamada a los pares ordenados de entrada al laberinto
         a = HEAD->x;
         b = HEAD->y;
         
